3b25d907bc1fc6e15f01d8d4ddbdcb9ea1795e71
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2021-01-14 21:32:26 +1000

    Utility to build tag paginator templates

---
+++ site/src/static/build_tags.ts
@@ -0,0 +1,49 @@
+import { loadPages } from "./build"
+import { CONTENT_DIR } from "./util"
+import fs from "fs"
+
+async function buildTagTemplates() {
+  const pages = await loadPages(`${CONTENT_DIR}/pages`)
+
+  // Get a list of tags used
+  const tags = pages.reduce((tagList, page) => {
+    if(page.options.has("tags")) {
+      (page.options.get("tags") as string[]).map(tag => {
+        if (!tagList.includes(tag)) tagList.push(tag)
+      })
+    }
+
+    return tagList
+  }, [])
+
+  await Promise.all(tags.map(buildTagTemplate))
+}
+
+async function buildTagTemplate(tag: string) {
+  const outputPath = `${CONTENT_DIR}/pages/tag/${tag}.md`
+  try {
+    await fs.promises.access(outputPath, fs.constants.F_OK)
+    return
+  } catch (err) {
+    await fs.promises.writeFile(outputPath, tagTemplateContent(tag))
+  }
+}
+
+function tagTemplateContent(tag: string) {
+  return `---
+title: "Tag: ${tag}"
+subtext: All articles tagged with ${tag}
+template: page_list
+paginator:
+  directory: posts
+  tag: ${tag}
+---
+
+Insert tag description here
+`
+}
+
+// Build a markdown template for each tag
+if(require.main === module) {
+  buildTagTemplates().then(_ => {})
+}
